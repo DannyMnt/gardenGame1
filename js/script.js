a04c0655d74f539bd9fcf2c5ecee09df55a556d4
DannyMnt <148209520+DannyMnt@users.noreply.github.com>
2023-11-09 10:04:08 +0100

    Create script.js
    trying so bad

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//MAKE THE MAGIC HAPPEN
+
+//Coded by Daniel
+//change
+
+//Coded by Tadeas
+const width = $("window").width();
+const heigth = $("window").heigth();
+
+let curPosX = $("#butterfly").offset().left;
+let curPosY = $("#butterfly").offset().top;
+
+function getRandomInt(max, min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+const clientWidth = $("window").
+$(document).ready(function(){
+    $("#butterfly").mouseover().animate({left:"+=getRandomInt(width-curPosX,-curPosX)",top:"+=getRandomInt(width-curPosY,-curPosY)"},fast);
+
+    $("#butterfly").animate({left:"+=(getRandomInt(30,-30))",top: "+=getRandomInt(30,-30)"},fast)
+});
+
+//Coded by Luca
+$
+
+
+//Coded by Jan
+
