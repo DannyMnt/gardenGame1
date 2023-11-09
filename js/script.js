eef41deee2a83293641e5305fb3e257c5654a684
DESKTOP-OGS76P7\munte <munteanudaniel098@gmail.com>
2023-11-09 09:28:44 +0100

    setup

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//MAKE THE MAGIC HAPPEN
+
+//Coded by Daniel
+
+
+
+//Coded by Tadeas
+const width = $("window").width();
+const heigth = $("window").heigth();
+$(document).ready(function(){
+    
+});
+
+//Coded by Luca
+$
+
+
+//Coded by Jan
