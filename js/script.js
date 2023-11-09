3293a41adfcff4214c5c1a0d859993dba1d221a3
T3Dyy1337 <juricektadeas@gmail.com>
2023-11-09 09:47:44 +0100

    test

--- js/script.js
+++ js/script.js
@@ -2,13 +2,24 @@
 
 //Coded by Daniel
 
-//test
+
 
 //Coded by Tadeas
 const width = $("window").width();
 const heigth = $("window").heigth();
+
+let curPosX = $("#butterfly").css(left);
+let curPosY = $("#butterfly").css(top);
+
+function getRandomInt(max, min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+const clientWidth = $("window").
 $(document).ready(function(){
-    
+    $("#butterfly").mouseover().animate({left:"+=getRandomInt(width-left)",right:"+=getRandomInt(width,width)"});
 });
 
 //Coded by Luca
@@ -16,3 +27,4 @@ $
 
 
 //Coded by Jan
+
